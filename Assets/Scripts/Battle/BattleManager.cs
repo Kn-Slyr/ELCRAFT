5a4703d43f8ff3732ec726e8e8b2f0e22fbf6c14
--replace-all <--unset>
2017-09-20 15:40:14 +0900

    abstract classes

---
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+	public static BattleManager instance = null;
+
+	public enum Turn { PLAYER, ENEMY, PROCEDURE };
+	public Turn turn;
+
+	public UnitInField[] unitsInField;
+	public UnitInField[] spawnQueue;
+
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/Battle/UnitForSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UnitForSpawn : MonoBehaviour
+{
+	DragNDrop drag;
+
+	bool CanSpawn()
+	{
+		return true;
+	}
+
+	void Spawn()
+	{
+
+	}
+}
