59ad1657bf82dba03e5147081c61fb36eb85bc13
--replace-all <--replace-all>
2017-09-28 14:51:09 +0900

    finish starter scene

--- Assets/Scripts/Battle/UnitForBattle.cs
+++ Assets/Scripts/Battle/UnitForBattle.cs
@@ -9,7 +9,6 @@ public abstract class UnitInField : MonoBehaviour
 		
 	}
 	
-	// Update is called once per frame
 	void Update ()
 	{
 		
--- Assets/Scripts/Starter/StarterPageLoader.cs
+++ Assets/Scripts/Starter/StarterPageLoader.cs
@@ -2,50 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StarterPageLoader : MonoBehaviour
 {
 	public GameObject popup;
 	public InputField inputId;
 	public InputField inputPswd;
-
+	
 	private bool popupValid;
 
 	void Start ()
 	{
 		popupValid = false;
 	}
-	
-	void Update ()
-	{
-		
-	}
 
 	private void OnMouseDown()
 	{
+		this.GetComponent<Collider2D>();
 		if (!popupValid)
-		{
-			popupValid = true;
 			CallLoginPopup();
-		}
+		else
+			ExitLoginPopup();
 	}
 
 	void CallLoginPopup()
 	{
+		popupValid = true;
 		popup.SetActive(true);
 	}
 
 	public void LoginProcess()
 	{
-		string id = inputId.text;
-		string pswd = inputPswd.text;
-		Debug.Log("( ID: " + id + ", PSWD: " + pswd + " ) LogIn!!");
+		string userID = inputId.text;
+		string userPwd = inputPswd.text;
+		Debug.Log("( ID: " + userID + ", PSWD: " + userPwd + " ) LogIn!!");
+
+		// @@have to make session
+
+		SceneManager.LoadScene("Lobby");
 	}
 
-	public void ExitLoginPopup()
+	void ExitLoginPopup()
 	{
-		popup.SetActive(false);
 		popupValid = false;
+		popup.SetActive(false);
 	}
 
 	public void SignupPorcess()
