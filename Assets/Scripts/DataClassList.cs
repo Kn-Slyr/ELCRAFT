ffe25b6957ed331a2abc7d6c49fe948de233c4b7
--replace-all <--replace-all>
2017-10-20 13:55:45 +0900

    Data File Manage Class

---
+++ Assets/Scripts/DataClassList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[Serializable]
+public struct StageData
+{
+	List<UnitSpawnData> doList;
+}
+
+[Serializable]
+public struct UnitSpawnData
+{
+	int turn;
+	int unitCode;
+	int boardX, boardY;
+}
+
+[Serializable]
+public struct CommanderSkillData
+{
+	int turn;
+	int skillCode;
+	int boardX, boardY;		// it is better for target unit?
+}
+
---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class DataManager
+{
+	string basicPath = Application.dataPath + "/Datas/";
+
+	public void BinarySave<T>(T target, string filePath)
+	{
+		BinaryFormatter formatter = new BinaryFormatter();
+		FileStream stream = new FileStream(basicPath + filePath, FileMode.Create);
+
+		formatter.Serialize(stream, target);
+		stream.Close();
+	}
+
+	public T BinaryLoad<T>(string filePath)
+	{
+		BinaryFormatter formatter = new BinaryFormatter();
+		FileStream stream = new FileStream(filePath, FileMode.Open);
+		T target = (T)formatter.Deserialize(stream);
+		stream.Close();
+
+		return target;
+	}
+}
